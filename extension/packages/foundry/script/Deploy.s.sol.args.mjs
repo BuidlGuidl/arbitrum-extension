fdf04899323ad77cc59cc0cd336db722f0894c2d
Elliott Alexander <escottalexander@gmail.com>
2025-07-01 14:43:11 -0400

    add foundry deploy script and update readme

---
+++ extension/packages/foundry/script/Deploy.s.sol.args.mjs
@@ -0,0 +1,5 @@
+export const deploymentsScriptsImports = `import { DeployArbAddressTableExample } from "./DeployArbAddressTableExample.s.sol";`;
+export const deploymentsLogic = `
+    DeployArbAddressTableExample deployArbAddressTableExample = new DeployArbAddressTableExample();
+    deployArbAddressTableExample.run();
+`;
